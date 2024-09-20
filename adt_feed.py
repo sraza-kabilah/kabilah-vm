79da1ee3b7f4c18d51a61a9339302e976895b10d
sarahraza007 <61799142+sarahraza007@users.noreply.github.com>
2024-09-20 18:06:25 -0400

    changed acks added print

--- adt_feed.py
+++ adt_feed.py
@@ -10,5 +10,7 @@ while True:
     print("{} connected".format( address ))
     time.sleep(0.2)
     response = client.recv(8112)
+    print(response)
+    default_ack = r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3" + "\n" + r"MSA|CA|2.3"
     if response != "":
-        client.send(b"\x06")
+        client.send(default_ack)
