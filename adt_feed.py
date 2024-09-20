8dfc83d780f48913ff6c4c138bd92de9af5b22de
sarahraza007 <61799142+sarahraza007@users.noreply.github.com>
2024-09-20 16:52:36 -0400

    chagned ack

--- adt_feed.py
+++ adt_feed.py
@@ -5,8 +5,7 @@ def create_default_ack():
     """
     Creates a default ACK message with generic values in case of an error.
     """
-    default_ack = r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3\r" + \
-                  r"MSA|CA|2.3\r"
+    default_ack = r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3" + "\n" + r"MSA|CA|2.3"
 
     return str(default_ack)
 
