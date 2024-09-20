6b80cfc3100533e6fab5989bc74063387b7e9a2c
sarahraza007 <61799142+sarahraza007@users.noreply.github.com>
2024-09-20 16:48:45 -0400

    changed ack

--- adt_feed.py
+++ adt_feed.py
@@ -5,8 +5,7 @@ def create_default_ack():
     """
     Creates a default ACK message with generic values in case of an error.
     """
-    default_ack = hl7.parse(r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3\r" +
-                        "MSA|CA|2.3\r")
+    default_ack = hl7.parse(r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3" + r"MSA|CA|2.3")
 
     return str(default_ack)
 
