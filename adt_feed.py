c142a36b551ada7125d76155e5c46228f5527dde
sarahraza007 <61799142+sarahraza007@users.noreply.github.com>
2024-09-23 14:42:34 -0400

    added timeout

--- adt_feed.py
+++ adt_feed.py
@@ -17,31 +17,42 @@ while True:
     server_socket.listen()
     client, address = server_socket.accept()
     print("{} connected".format(address))
-    time.sleep(0.2)
     
-    # Receive response
-    response = client.recv(8112)
-    
-    # Check for non-empty response
-    if response != b'':
-        print(response)
+    try:
+        # Set a timeout for receiving data
+        client.settimeout(60)
         
-        # Send default ACK response
-        default_ack = "\x0b" + r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3" + "\n" + r"MSA|CA|2.3" + "\r\x1c\r"
-        client.send(default_ack.encode())
+        # Receive response
+        response = client.recv(8112)
         
-        # Insert response into database
-        try:
-            cursor.execute('''
-                INSERT INTO adt_feed_raw (raw_message) 
-                VALUES (?)
-            ''', (response.decode(),))
-            cnxn.commit()
-        except Exception as e:
-            print(f"Database insert error: {e}")
+        # Check for non-empty response
+        if response:
+            print(response)
+            
+            # Send default ACK response
+            default_ack = "\x0b" + r"MSH|^~\&|||HIHLSEA-230502|EAGLE 2000|20240920010246||ACK|2.3|T|2.3" + "\n" + r"MSA|CA|2.3" + "\r\x1c\r"
+            client.send(default_ack.encode())
+            
+            # Insert response into database
+            try:
+                cursor.execute('''
+                    INSERT INTO adt_feed_raw (raw_message) 
+                    VALUES (?)
+                ''', (response.decode(),))
+                cnxn.commit()
+            except Exception as e:
+                print(f"Database insert error: {e}")
+        
+        # Give some time before closing the connection
+        time.sleep(10)
     
-    # Close client connection
-    client.close()
+    except socket.timeout:
+        print("Client timed out.")
+    
+    finally:
+        # Close client connection
+        client.close()
+        # Properly close the database connection when done
+        cnxn.close()
 
-# Properly close the database connection when done
-cnxn.close()
+
